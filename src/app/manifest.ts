ab456dbbe593e0a43ce5ac096e31f6a786a33cf6
DelcioFDNeto <delsinho2005@gmail.com>
2026-01-28 15:30:05 -0300

    PWA ATIVADO

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,11 +3,26 @@ import { MetadataRoute } from 'next';
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'SGM Shineray',
-    short_name: 'SGM By Sabel',
+    short_name: 'SGM',
     description: 'Sistema de Gestão de Montagem e Qualidade',
-    start_url: '/login',
-    display: 'standalone',
+    start_url: '/', // Mudei para raiz '/' para evitar erros de redirecionamento
+    display: 'standalone', // ISSO é o que tira a barra de navegação
     background_color: '#ffffff',
     theme_color: '#2563eb',
+    orientation: 'portrait', // Força modo retrato no celular
+    icons: [
+      {
+        src: '/icon-192.png', // Caminho na pasta public
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable'
+      },
+      {
+        src: '/icon-512.png', // Caminho na pasta public
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any'
+      },
+    ],
   };
 }
