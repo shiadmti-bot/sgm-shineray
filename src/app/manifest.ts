f4232c9242376832949b27b1aebc5ad3bffd506f
DelcioFDNeto <delsinho2005@gmail.com>
2026-01-28 14:57:44 -0300

    Update: Qualidade do icone melhorada

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,23 +3,11 @@ import { MetadataRoute } from 'next';
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'SGM Shineray',
-    short_name: 'SGM',
+    short_name: 'SGM By Sabel',
     description: 'Sistema de Gestão de Montagem e Qualidade',
-    start_url: '/login', // Ao abrir o app, vai direto pro login
-    display: 'standalone', // Remove a barra do navegador (fio app nativo)
+    start_url: '/login',
+    display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#2563eb', // Azul da Shineray
-    icons: [
-      {
-        src: '/web-app-manifest-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/web-app-manifest-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
+    theme_color: '#2563eb',
   };
 }
